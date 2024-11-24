8ecee3b93b10cf0bec661f3f241a10748ae30d4b
shawkyGalal <shawky.foda@gmail.com>
2024-11-24 15:27:40 +0300

    org.springframework.security.crypto.codec.Base64 is deprecated use java.util.Base64  instead

--- src/main/com/smartvalue/google/iam/GoogleWebAppCredential.java
+++ src/main/com/smartvalue/google/iam/GoogleWebAppCredential.java
@@ -3,7 +3,6 @@ package com.smartvalue.google.iam ;
 
 import java.nio.charset.Charset;
 
-import org.springframework.security.crypto.codec.Base64;
 
 import com.google.gson.Gson;
 import com.mashape.unirest.http.HttpResponse;
@@ -22,7 +21,7 @@ public class GoogleWebAppCredential extends com.smartvalue.google.iam.auto.Googl
 		String tokenUrl = this.getTokenUri() ; 
 		HttpResponse<String> accessTokenResponse = Unirest.post(tokenUrl)
 		  .header("Content-Type", "application/x-www-form-urlencoded")
-		  .header("Authorization", "Basic "+ new String(Base64.encode((clientId + ":" + clientISectet).getBytes()), Charset.forName("UTF-8")))
+		  .header("Authorization", "Basic "+ new String(java.util.Base64.getEncoder().encode((clientId + ":" + clientISectet).getBytes()), Charset.forName("UTF-8")))
 		  .field("grant_type", "authorization_code")
 		  .field("code", authCode)
 		  .field("redirect_uri", redirectUri )  //"https://apigeeadmin.moj.gov.sa:8443/ResourceManagerWeb/loginWithGoogle/authCodeHandler.jsp")
